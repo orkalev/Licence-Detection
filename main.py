ecc26e79293710e824e32864a3e783a0c8824e84
Or Kalev <orkalev@gmail.com>
2021-02-19 17:57:50 +0200

    Friday Commit

--- main.py
+++ main.py
@@ -7,70 +7,6 @@ import tkinter.filedialog as tkFileDialog
 import numpy as np
 
 
-
-def order_points(pts):
-    # initialzie a list of coordinates that will be ordered
-    # such that the first entry in the list is the top-left,
-    # the second entry is the top-right, the third is the
-    # bottom-right, and the fourth is the bottom-left
-    rect = np.zeros((4, 2), dtype="float32")
-
-    # the top-left point will have the smallest sum, whereas
-    # the bottom-right point will have the largest sum
-    s = pts.sum(axis=1)
-    rect[0] = pts[np.argmin(s)]
-    rect[2] = pts[np.argmax(s)]
-
-    # now, compute the difference between the points, the
-    # top-right point will have the smallest difference,
-    # whereas the bottom-left will have the largest difference
-    diff = np.diff(pts, axis=1)
-    rect[1] = pts[np.argmin(diff)]
-    rect[3] = pts[np.argmax(diff)]
-
-    # return the ordered coordinates
-    return rect
-
-
-def four_point_transform(image, pts):
-    # obtain a consistent order of the points and unpack them
-    # individually
-    rect = order_points(pts)
-    (tl, tr, br, bl) = rect
-
-    # compute the width of the new image, which will be the
-    # maximum distance between bottom-right and bottom-left
-    # x-coordiates or the top-right and top-left x-coordinates
-    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
-    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
-    maxWidth = max(int(widthA), int(widthB))
-
-    # compute the height of the new image, which will be the
-    # maximum distance between the top-right and bottom-right
-    # y-coordinates or the top-left and bottom-left y-coordinates
-    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
-    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
-    maxHeight = max(int(heightA), int(heightB))
-
-    # now that we have the dimensions of the new image, construct
-    # the set of destination points to obtain a "birds eye view",
-    # (i.e. top-down view) of the image, again specifying points
-    # in the top-left, top-right, bottom-right, and bottom-left
-    # order
-    dst = np.array([
-        [0, 0],
-        [maxWidth - 1, 0],
-        [maxWidth - 1, maxHeight - 1],
-        [0, maxHeight - 1]], dtype="float32")
-
-    # compute the perspective transform matrix and then apply it
-    M = cv2.getPerspectiveTransform(rect, dst)
-    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
-
-    # return the warped image
-    return warped
-
-
 def filter_contrast(image):
     contrastPrsent = 10
     grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -83,12 +19,10 @@ def filter_contrast(image):
     contrastPrsent *= (acc[-1] / 100.0)
     contrastPrsent /= 2.0
 
-    # Locate left cut
     minGray = 0
     while acc[minGray] < contrastPrsent:
         minGray += 1
 
-    # Locate right cut
     maxGray = len(grayscaleHistogram) - 1
     while acc[maxGray] >= (acc[-1] - contrastPrsent):
         maxGray -= 1
@@ -130,20 +64,18 @@ def detect_plant(originalImage):
             hsvColorImage = cv2.cvtColor(cropImageYellow, cv2.COLOR_BGR2HSV)
             yellowImage = cv2.inRange(hsvColorImage, np.array([20, 100, 100]), np.array([30, 255, 255]))
 
-            # Compute yellow density
-            yellow_summation = yellowImage.sum()
-
             # Condition on yellow color density in the crop
-            if yellow_summation > 255 * cropImage.shape[0] * cropImage.shape[0] * 0.4:
+            if yellowImage.sum() > 255 * cropImage.shape[0] * cropImage.shape[0] * 0.4:
 
                 # Make a crop from the gray image
-                crop_gray = imgray[y:y + h, x:x + w]
-                crop_gray = crop_gray.astype('uint8')
-######## if we reach here : we now have the yellow image crop of the plate
+                corpImageGray = imgray[y:y + h, x:x + w]
+                corpImageGray = corpImageGray.astype('uint8')
+
+                # At this point we know that the crop image is the yellow plant
 
 
                 # Detect chars inside yellow crop with specefic dimension and area
-                th = cv2.adaptiveThreshold(crop_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,
+                th = cv2.adaptiveThreshold(corpImageGray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,
                                            11, 2)   # make a mask(black and white) img
                                                     # from the croped yellow plate
                 contours2, her = cv2.findContours(th, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -162,7 +94,32 @@ def detect_plant(originalImage):
                 if 20 > chars > 4:
                     box = np.int0(cv2.boxPoints(cv2.minAreaRect(contour)))
                     pts = np.array(box)
-                    adjusted = filter_contrast(four_point_transform(copyImage, pts))
+                    # Order the rect corners
+                    rect = np.zeros((4, 2), dtype="float32")
+                    s = pts.sum(axis=1)
+                    rect[0] = pts[np.argmin(s)]
+                    rect[2] = pts[np.argmax(s)]
+                    diff = np.diff(pts, axis=1)
+                    rect[1] = pts[np.argmin(diff)]
+                    rect[3] = pts[np.argmax(diff)]
+
+                    # Transform the points
+                    (tl, tr, br, bl) = rect
+                    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+                    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+                    maxWidth = max(int(widthA), int(widthB))
+                    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+                    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+                    maxHeight = max(int(heightA), int(heightB))
+                    dst = np.array([
+                        [0, 0],
+                        [maxWidth - 1, 0],
+                        [maxWidth - 1, maxHeight - 1],
+                        [0, maxHeight - 1]], dtype="float32")
+                    M = cv2.getPerspectiveTransform(rect, dst)
+                    adjusted = cv2.warpPerspective(copyImage, M, (maxWidth, maxHeight))
+
+                    #adjusted = filter_contrast(four_point_transform(copyImage, pts))
                     plate = cv2.cvtColor(adjusted, cv2.COLOR_BGR2GRAY)
                     licenseNum = pytesseract.image_to_string(plate, config='--psm 13 -c tessedit_char_whitelist=0123456789')
                     # Put the license number on the photo
@@ -183,7 +140,6 @@ def importVideo():
         print("Error opening video stream or file")
 
     while (cap.isOpened()):
-        # Capture frame-by-frame
         ret, frame = cap.read()
         if ret == True:
 
@@ -254,7 +210,7 @@ def importImage():
             sug_delek_nm = record["sug_delek_nm"]
             horaat_rishum = record["horaat_rishum"]
             kinuy_mishari = record["kinuy_mishari"]
-            canvas1.create_text(730, 50, text=str(mispar_rechev))
+            canvas1.create_text(740, 50, text=str(mispar_rechev))
             canvas1.create_text(770, 70, text=tozeret_nm)
             canvas1.create_text(710, 90, text=ramat_gimur)
             canvas1.create_text(760, 110, text=ramat_eivzur_betihuty)
